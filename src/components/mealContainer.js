ac8f1cc7b7e8cd8cd701a2a11a17aea21e61c392
Selina <selina.dxl@gmail.com>
2022-10-16 11:24:03 -0400

    move getMeal to meal component. display for array of meals

--- src/components/mealContainer.js
+++ src/components/mealContainer.js
@@ -1,46 +1,29 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import { Meal } from "./meal";
-import { Ingredients } from "./ingredients";
-import { Instructions } from "./instructions";
 
 export const MealContainer = () => {
-    const [mealName, setName] = useState('')
-    const [mealImg, setImg] = useState('')
-    const [ingredients, setIngredients] = useState([])
-    const [measures, setMeasures] = useState([])
-    const [instructions, setInstructions] = useState('')
+  const meals = ['BeaverTails', 'Breakfast Potatoes', 'Canadian Butter Tarts', 'Montreal Smoked Meat', 'Nanaimo Bars']
+  // const getMeals = async () => {
+  //   fetch("https://www.themealdb.com/api/json/v1/1/filter.php?a=Canadian")
+  //       .then((response) => response.json())
+  //       .then((json) => {
+  //         for(const meal of json.meals)
+  //           meals.push(meal.strMeal)
+  //       })
+  //       .catch((error) => {
+  //       console.error(error);
+  //       });
+  // }
 
-    const getMeal = async () => {
-        fetch("https://www.themealdb.com/api/json/v1/1/search.php?s=Fettuccine")
-            .then((response) => response.json())
-            .then((json) => {
-                let meal = json.meals[0]
-                setName(meal.strMeal)
-                setImg(meal.strMealThumb)
-                setInstructions(meal.strInstructions)
-                let values = Object.values(meal)
-                for(let i=9; i<29; i++){
-                    if(values[i] === ''){
-                        break
-                    }
-                    ingredients.push(values[i])
-                    measures.push(values[i+20])
-                }
-            })
-            .catch((error) => {
-            console.error(error);
-            });
-    }
-
-    useEffect(() => {
-        getMeal()
-    }, [])
+  // useEffect(() => {
+  //     getMeals()
+  // }, [])
 
   return (
     <div>
-      <Meal name={mealName} img={mealImg} />
-      <Ingredients ingredients={ingredients} measures={measures}/>
-      <Instructions instructions={instructions} />
+      {meals.map(
+        meal => <Meal name={meal} />
+      )}
     </div>
   );
 };
