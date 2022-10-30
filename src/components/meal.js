8af170098c76143fbaef9bbdff6571becc1ee73a
Selina <selina.dxl@gmail.com>
2022-10-30 18:07:46 -0400

    move api call to mealContainer. store meals in array. show one meal at a time (wip)

--- src/components/meal.js
+++ src/components/meal.js
@@ -1,81 +1,18 @@
-import React, { useEffect, useState } from 'react'
 import { Ingredients } from "./ingredients";
 import { Instructions } from "./instructions";
 
 export const Meal = ({
-    name
+    name,
+    img,
+    ingredients,
+    measures,
+    instructions,
 }) => {
-    const [mealImg, setImg] = useState('')
-    const [ingredients, setIngredients] = useState([])
-    const [measures, setMeasures] = useState([])
-    const [instructions, setInstructions] = useState('')
-    const ingredientKeys = ["strIngredient1",
-                            "strIngredient2",
-                            "strIngredient3",
-                            "strIngredient4",
-                            "strIngredient5",
-                            "strIngredient6",
-                            "strIngredient7",
-                            "strIngredient8",
-                            "strIngredient9",
-                            "strIngredient10",
-                            "strIngredient11",
-                            "strIngredient12",
-                            "strIngredient13",
-                            "strIngredient14",
-                            "strIngredient15",
-                            "strIngredient16",
-                            "strIngredient17",
-                            "strIngredient18",
-                            "strIngredient19",
-                            "strIngredient20"]
-    const measureKeys = ["strMeasure1",
-                          "strMeasure2",
-                          "strMeasure3",
-                          "strMeasure4",
-                          "strMeasure5",
-                          "strMeasure6",
-                          "strMeasure7",
-                          "strMeasure8",
-                          "strMeasure9",
-                          "strMeasure10",
-                          "strMeasure11",
-                          "strMeasure12",
-                          "strMeasure13",
-                          "strMeasure14",
-                          "strMeasure15",
-                          "strMeasure16",
-                          "strMeasure17",
-                          "strMeasure18",
-                          "strMeasure19",
-                          "strMeasure20"]
-    const getMeal = async () => {
-        fetch("https://www.themealdb.com/api/json/v1/1/search.php?s="+name)
-            .then((response) => response.json())
-            .then((json) => {
-                let meal = json.meals[0]
-                setImg(meal.strMealThumb)
-                setInstructions(meal.strInstructions)
-                for(let i=0; i<20; i++){
-                  if(meal[ingredientKeys[i]] !== ''){
-                    ingredients.push(meal[ingredientKeys[i]])
-                    measures.push(meal[measureKeys[i]])
-                  }
-                }
-            })
-            .catch((error) => {
-            console.error(error);
-            });
-    }
-
-    useEffect(() => {
-        getMeal()
-    }, [])
 
     return(
         <div>
             <h1>{name}</h1>
-            <img src={mealImg}/>
+            <img src={img}/>
             <Ingredients ingredients={ingredients} measures={measures}/>
             <Instructions instructions={instructions} />
         </div>
--- src/components/mealContainer.js
+++ src/components/mealContainer.js
@@ -1,29 +1,130 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { Meal } from "./meal";
+import '../App.css';
 
 export const MealContainer = () => {
-  const meals = ['BeaverTails', 'Breakfast Potatoes', 'Canadian Butter Tarts', 'Montreal Smoked Meat', 'Nanaimo Bars']
-  // const getMeals = async () => {
-  //   fetch("https://www.themealdb.com/api/json/v1/1/filter.php?a=Canadian")
-  //       .then((response) => response.json())
-  //       .then((json) => {
-  //         for(const meal of json.meals)
-  //           meals.push(meal.strMeal)
-  //       })
-  //       .catch((error) => {
-  //       console.error(error);
-  //       });
-  // }
+  const [meals, setMeals] = useState([]);
+  const [index, setIndex] = useState(0);
+  const ingredientKeys = [
+    "strIngredient1",
+    "strIngredient2",
+    "strIngredient3",
+    "strIngredient4",
+    "strIngredient5",
+    "strIngredient6",
+    "strIngredient7",
+    "strIngredient8",
+    "strIngredient9",
+    "strIngredient10",
+    "strIngredient11",
+    "strIngredient12",
+    "strIngredient13",
+    "strIngredient14",
+    "strIngredient15",
+    "strIngredient16",
+    "strIngredient17",
+    "strIngredient18",
+    "strIngredient19",
+    "strIngredient20",
+  ];
+  const measureKeys = [
+    "strMeasure1",
+    "strMeasure2",
+    "strMeasure3",
+    "strMeasure4",
+    "strMeasure5",
+    "strMeasure6",
+    "strMeasure7",
+    "strMeasure8",
+    "strMeasure9",
+    "strMeasure10",
+    "strMeasure11",
+    "strMeasure12",
+    "strMeasure13",
+    "strMeasure14",
+    "strMeasure15",
+    "strMeasure16",
+    "strMeasure17",
+    "strMeasure18",
+    "strMeasure19",
+    "strMeasure20",
+  ];
 
-  // useEffect(() => {
-  //     getMeals()
-  // }, [])
+  const getMeals = async () => {
+    fetch("https://www.themealdb.com/api/json/v1/1/filter.php?a=Canadian")
+      .then((response) => response.json())
+      .then((json) => {
+        const mealTemp = [];
+        for (const meal of json.meals) mealTemp.push(meal);
+        setMeals(mealTemp);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
+
+  const getMealDetails = async (currentMeal, name) => {
+    fetch("https://www.themealdb.com/api/json/v1/1/search.php?s=" + name)
+      .then((response) => response.json())
+      .then((json) => {
+        let meal = json.meals[0];
+        currentMeal.strMealThumb = meal.strMealThumb;
+        currentMeal.strInstructions = meal.strInstructions;
+        currentMeal.ingredients = [];
+        currentMeal.measures = [];
+        for (let i = 0; i < 20; i++) {
+          if (meal[ingredientKeys[i]] !== "") {
+            currentMeal.ingredients.push(meal[ingredientKeys[i]]);
+            currentMeal.measures.push(meal[measureKeys[i]]);
+          }
+        }
+        setMeals(meals)
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
+
+  useEffect(() => {
+    getMeals();
+  }, []);
+
+  useEffect(() => {
+    meals.map(
+      (meal) =>
+        meals && !meal.strInstructions && getMealDetails(meal, meal.strMeal)
+    );
+  })
+
+  const getPrev = () => {
+    const isFirst = index === 0
+    const newIndex = isFirst ? meals.length-1 : index-1
+    setIndex(newIndex)
+  };
+
+  const getNext = () => {
+    const isLast = index === meals.length-1
+    const newIndex = isLast ? 0 : index+1
+    setIndex(newIndex)
+  };
+
+  //meals array is filled successfully, but display causes error. 
+  //Uncaught TypeError: Cannot read properties of undefined (reading 'ingredients')
+  //no error on save, only manual refresh
+
+  //Uncaught Error: Too many re-renders.
 
   return (
     <div>
-      {meals.map(
-        meal => <Meal name={meal} />
-      )}
+      {/* <button className='prev' onClick={getPrev()}>previous</button> */}
+      {/* <Meal
+        name={meals[index].strMeal}
+        img={meals[index].strMealThumb}
+        ingredients={meals[index].ingredients}
+        measures={meals[index].measures}
+        instructions={meals[index].strInstructions}
+      /> */}
+      {/* <button className='next' onClick={getNext()}>next</button> */}
     </div>
   );
 };
